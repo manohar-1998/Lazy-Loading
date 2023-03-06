9fd2722d27235ee8931fb6ea291f2a4d260463b2
Manohar Gorla <manohar.gorla@quadratyx.com>
2023-03-06 11:49:39 +0530

    Adds some changes to UI code

--- src/app/user-management/user-management-routing.module.ts
+++ src/app/user-management/user-management-routing.module.ts
@@ -6,6 +6,6 @@ const routes: Routes = [{ path: '', component: UserManagementComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class UserManagementRoutingModule { }
+export class UserManagementRoutingModule {}
